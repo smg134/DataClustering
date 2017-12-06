53de8e5043b00c3ad60b855ab4d73dafeea1e11e
Sean Gaffney <smg134@zips.uakron.edu>
2017-12-06 02:42:54 -0500

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -10,14 +10,15 @@ function clusters() {
 
 function redirect() {
     var num = document.getElementById("number").value;
-    if (num == 2) window.location = "http://dataclustering2.azurewebsites.net/data/2.png";
-    if (num == 3) window.location = "http://dataclustering2.azurewebsites.net/data/3.png";
-    if (num == 4) window.location = "http://dataclustering2.azurewebsites.net/data/4.png";
-    if (num == 5) window.location = "http://dataclustering2.azurewebsites.net/data/5.png";
-    if (num == 6) window.location = "http://dataclustering2.azurewebsites.net/data/6.png";
-    if (num == 7) window.location = "http://dataclustering2.azurewebsites.net/data/7.png";
-    if (num == 8) window.location = "http://dataclustering2.azurewebsites.net/data/8.png";
-    if (num == 9) window.location = "http://dataclustering2.azurewebsites.net/data/9.png";
-    if (num == 10) window.location = "http://dataclustering2.azurewebsites.net/data/10.png";
+    if (num == 2) document.getElementById("image").src = "http://dataclustering2.azurewebsites.net/data/2.png";
+    if (num == 3) document.getElementById("image").src = "http://dataclustering2.azurewebsites.net/data/3.png";
+    if (num == 4) document.getElementById("image").src = "http://dataclustering2.azurewebsites.net/data/4.png";
+    if (num == 5) document.getElementById("image").src = "http://dataclustering2.azurewebsites.net/data/5.png";
+    if (num == 6) document.getElementById("image").src = "http://dataclustering2.azurewebsites.net/data/6.png";
+    if (num == 7) document.getElementById("image").src = "http://dataclustering2.azurewebsites.net/data/7.png";
+    if (num == 8) document.getElementById("image").src = "http://dataclustering2.azurewebsites.net/data/8.png";
+    if (num == 9) document.getElementById("image").src = "http://dataclustering2.azurewebsites.net/data/9.png";
+    if (num == 10) document.getElementById("image").src = "http://dataclustering2.azurewebsites.net/data/10.png";
+    document.body.style.cursor = "default";
     return false;
 }
