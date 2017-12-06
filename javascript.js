ce84dbcc8997f2aacd918cee035a57bb56bf96ac
Sean Gaffney <smg134@zips.uakron.edu>
2017-12-06 02:26:19 -0500

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 function clusters() {
-    num = document.getElementById("clusters").value;
+    var num = document.getElementById("clusters").value;
     if (num < 2 || num > 10) {
         document.getElementById("tag").innerHTML = "Value must be between 2 and 10.";
     }
@@ -9,7 +9,7 @@ function clusters() {
 }
 
 function redirect() {
-    num = document.getElementById("clusters").value;
+    var num = document.getElementById("clusters").value;
     if (num == 2) window.location = "http://dataclustering2.azurewebsites.net/data/2.png";
     if (num == 3) window.location = "http://dataclustering2.azurewebsites.net/data/3.png";
     if (num == 4) window.location = "http://dataclustering2.azurewebsites.net/data/4.png";
